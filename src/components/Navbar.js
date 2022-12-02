98280c3646e17cd39615f71258e1e78c759a9289
joyce <joycemendes.jm79@gmail.com>
2022-12-02 15:34:17 -0300

    links para autenticados e não aicados e não autenticados

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 
 import { NavLink } from "react-router-dom";
+import { useAuthentication } from "../hooks/useAuthentication";
+import { useAuthValue } from "../context/AuthContext"
 
 //css
 import styles from "./Navbar.module.css";
 
 
 const Navbar = () => {
+
+    const { user } = useAuthValue();
+
+
   return (
     <nav className={ styles.navbar }>
         <NavLink to="/" className={ styles.brand }>
@@ -16,12 +22,28 @@ const Navbar = () => {
             <li>
                 <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>Home</NavLink>
             </li>
-            <li>
-                <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Entrar</NavLink>
-            </li>
-            <li>
-                <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
-            </li>
+            {!user && (
+                <>
+                    <li>
+                        <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Entrar</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
+                    </li>
+                </>
+            )}
+            {/*Usuario logado */}
+            {user && (
+                <>
+                    <li>
+                        <NavLink to="/posts/create" className={({isActive}) => (isActive ? styles.active : "")}>Novo Post</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard" className={({isActive}) => (isActive ? styles.active : "")}>Dashboard</NavLink>
+                    </li>
+                </>
+            )}
+
 
             <li>
                 <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>Sobre</NavLink>
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import styles from "./Dashboard.module.css";
+
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1>Dasboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
