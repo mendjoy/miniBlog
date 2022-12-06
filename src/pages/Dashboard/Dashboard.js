0347ab4af8cb272b34aabef66f0b3450c73eff62
joyce <joycemendes.jm79@gmail.com>
2022-12-06 18:56:27 -0300

    css dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -26,7 +26,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <div className={ styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
 
@@ -37,12 +37,12 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={ styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
           </div>
        
-        {posts && posts.map((post) => <div key={ post.id }>
+        {posts && posts.map((post) => <div key={ post.id } className={styles.post_row}>
           <p>{ post.title }</p>
           <div>
             <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
