a349c74b0d9cacb2b3328748a23df7a326a247aa
joyce <joycemendes.jm79@gmail.com>
2022-12-06 13:27:12 -0300

    componente de post detail e css

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const PostDetail = ({post}) => {
   return (
-    <div>
+    <div className={ styles.post_detail}>
         
         <img src={ post.image} alt={ post.title } />
         <h2>{ post.title }</h2>
-        <p> { post.createdBy }</p>
-        <div>
+        <p className={  styles.createdBy }> { post.createdBy }</p>
+        <div className={ styles.tags }>
             { post.tagsArray.map((tag) => (
-                <p key={ tag }><span>#</span>{tag }</p>
+                <p key={tag}><span>#</span>{ tag } </p>
             ))}
         </div>
         <Link className="btn btn-outline" to={ `/posts/${post.id}` }>Ler</Link>
